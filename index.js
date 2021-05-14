2d1dd23b5e61f6535f7160bf4d85fd5e748d18d8
Mariam  Khamo <mariamw736@gmail.com>
2021-05-14 10:35:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 // Your code goes here
-let text = document.getElementById("text") ;
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("The DOM has loaded");
-       text.innerHtml = "This is really cool!";
- 
-});
-
- console.log(text);
-  
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("text").innerHTML = "This is really cool!";
+   })
