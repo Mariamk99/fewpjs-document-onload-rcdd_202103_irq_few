a3e38c75bb605f80c3368f21bc50aecf39fcdf1f
Mariam  Khamo <mariamw736@gmail.com>
2021-05-14 10:27:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("The DOM has loaded");
-});
    let text = document.getElementById('text') . innerHtml = "This is really cool!";
+  console.log(text);
+});
+  
