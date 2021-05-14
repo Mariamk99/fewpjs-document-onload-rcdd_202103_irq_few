1617b950d9628fe58121d17c938b57f78e111b7c
Mariam  Khamo <mariamw736@gmail.com>
2021-05-14 10:29:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
    let text = document.getElementById('text') . innerHtml = "This is really cool!";
-  console.log(text);
+ 
 });
+ console.log(text);
   
